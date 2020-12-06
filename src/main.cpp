2d67b59122cc7f53420e89c66dfc5ca551e7f043
Nebojsa Koturovic <necer95@gmail.com>
2020-12-06 23:50:44 +0100

    GLES shader testing

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,11 @@ void die(std::string_view msg) {
 
 int main() {
     glfwInit();
+    // if uncomment Hints raspberry build not working
     // Disable some old features like glBegin/glEnd etc...
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -42,22 +43,22 @@ int main() {
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
         die("Failed to initialize GLAD");
 
-	constexpr const char* vertexShaderSource = 
-		"#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main()\n"
-		"{\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"}\0";
+    constexpr const char* vertexShaderSource = 
+        "#version 300 es\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
 
-	constexpr const char* fragmentShaderSource = 
-		"#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"void main()\n"
-		"{\n"
-		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-		"}\n\0";
-
+    constexpr const char* fragmentShaderSource = 
+        "#version 300 es\n"
+        "out mediump vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n\0";
+    
     // build and compile our shader program
     int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
