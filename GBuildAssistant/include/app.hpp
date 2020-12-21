d5c5d3b882da6853862146dcc85774d52f49c681
Nebojša Koturović <necer95@gmail.com>
2020-12-21 19:57:28 +0100

    Building project as static library, project structure reorganized

---
+++ GBuildAssistant/include/app.hpp
@@ -0,0 +1,22 @@
+#ifndef GBA_APP_HPP
+#define GBA_APP_HPP
+#include <string_view>
+
+namespace gba {
+
+struct AppParams {
+    std::string_view window_name = "GBuildAssistant App";
+    int window_width = 800;
+    int window_height = 600;
+};
+
+class App {
+public:
+    App(AppParams params);
+    void run();
+    ~App();
+};
+
+} // ns gba
+
+#endif
---
+++ demos/running_gba_app.cpp
@@ -0,0 +1,13 @@
+#include "app.hpp"
+
+int main() {
+
+    gba::App app({
+        .window_width = 1024,
+        .window_height = 768
+    });
+
+    app.run();
+
+    return 0;
+}
