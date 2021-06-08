4c2d5776e50d307fb6308a31468422bcdf19b135
Nebojša Koturović <necer95@gmail.com>
2021-06-08 23:58:10 +0200

    conan: Add packges to conan_packages folder
    BinTray is for paid users only, create backup packages in conan_packages

--- cmake/RunConan.cmake
+++ cmake/RunConan.cmake
@@ -14,12 +14,13 @@ macro(run_conan)
         https://api.bintray.com/conan/bincrafters/public-conan
     )
 
-    conan_add_remote(
-        NAME
-        kotur
-        URL
-        https://api.bintray.com/conan/kotur/public-conan
-    )
+    # bintray is now for paid users only
+    #conan_add_remote(
+        #NAME
+        #kotur
+        #URL
+        #https://api.bintray.com/conan/kotur/public-conan
+    #)
 
     conan_cmake_run (
         CONANFILE conanfile.txt
